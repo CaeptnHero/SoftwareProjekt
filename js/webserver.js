602b9c6679a2dc9baa50ddcc3b563f194281f444
CaeptnHero <63815774+CaeptnHero@users.noreply.github.com>
2022-05-11 15:38:17 +0200

    Update webserver.js
    test Fileupload

--- js/webserver.js
+++ js/webserver.js
@@ -1,19 +1,47 @@
 // import Node.js core module
 var http = require("http");
 
+// Fileupload
+var formidable = require("formidable");
+var fs = require("fs");
+
 // create Server
 var server = http.createServer(function (request, response) {
+    if (request.url == "/fileupload") {
+        var form = new formadible.IncomingForm();
+        form.parse(request, function (error, fields, files) {
+            var oldpath = files.file.parth;
+            var newpath = _dirname + "/files" + files.file.name;
+            fs.rename(oldpath, newpath, function (error) {
+                if (error)
+                    throw error;
+                response.write("File uploaded and moved");
+                response.end();
+            })
+        })
+    }
+    else {
+        response.writeHead(200, { "content-type": "text/html; charset=utf-8" });
+        
+        var body = 
+            "<form action= ''fileupload method='post' enctype= 'multipart/form-data'>"+
+            "<input type= 'file' name= 'file'><br>"+
+            "<input type= 'submit'>"+
+            "</form";
+        var htmlResponse = getHTMLStruct ("NameHTMLDatei", body);
+        response.end(htmlResponse);
+
+    }
+
+    
+
+
+ /*
     response.writeHead(200, { "content-type": "text/html; charset=utf-8" });
-    /*
     Echo Test
     response.write("Hello");
     response.end("World");
     */
-
-    var body = 
-
-
-
 })
 
 // listen for any incoming requests
