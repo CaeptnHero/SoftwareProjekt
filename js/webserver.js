791bcc72f43dc756bd980d8cbfa7ca1e0413bef1
CaeptnHero <63815774+CaeptnHero@users.noreply.github.com>
2022-05-11 14:59:40 +0200

    Update webserver.js
    Now the Webserver works with a simple echo response

--- js/webserver.js
+++ js/webserver.js
@@ -1,8 +1,21 @@
-var http = require ("http");
-http.createServer(function(request, response){
-    response.write.Head(200, {"content-type" : "text/html; charset=utf-8"});
+// import Node.js core module
+var http = require("http");
 
-    response.write("Hello")
+// create Server
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, { "content-type": "text/html; charset=utf-8" });
+    /*
+    Echo Test
+    response.write("Hello");
     response.end("World");
-}).listen(8080, "127.0.0.1");
+    */
+
+    var body = 
+
+
+
+})
+
+// listen for any incoming requests
+server.listen(8080, "127.0.0.1");
 console.log("Server is running!");
