d84a913d2a7c0cb09127b183a4e072ba5130a4b4
CaeptnHero <63815774+CaeptnHero@users.noreply.github.com>
2022-05-12 13:34:04 +0200

    Update dataBase.js

--- js/dataBase.js
+++ js/dataBase.js
@@ -1,10 +1,21 @@
-var mongo = require ("mongodb").MongoClient;
-mongo.connect ("mongo://localhost:27017"), {userNewUrlParser: true},
-function (error, client) {
-    if (error) {
-        throw error;
-    }else{
-        const collUser = client.db ("newDB").collection("user");
-        client.close();
+var mongo = require("mongodb").MongoClient;
+mongo.connect("mongo://localhost:27017"), { userNewUrlParser: true },
+    function (error, client) {
+        if (error) {
+            throw error;
+        } else {
+            const collUser = client.db("newDB").collection("user");
+            
+            // Create Datensatz
+            createData(colUser,
+                [{ vorname: "Max", name: "Mustermann" }]);
+
+            client.close();
+        }
+    };
+
+function callBackFunction(result, message = "") {
+    if (message !== "") {
+        console.log(message + ":" + result);
     }
 }
